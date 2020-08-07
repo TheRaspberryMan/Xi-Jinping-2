18080a845dabd0db0c25172c6a6e909425b9d8bc
TheRaspberryMan <theofreilly@gmail.com>
2020-08-07 17:58:31 -0400

    updated main to have muting

--- main.py
+++ main.py
@@ -28,6 +28,25 @@ __location__ = os.path.realpath(
 
 user_data_path = 'user_data.json'
 
+
+# adds a member to the user data json with 0 for money, offences, and xp
+def add_member_to_json(member):
+
+    # loads the data
+    with open(user_data_path, 'r') as user_data_file:
+        user_data = json.load(user_data_file)
+        money_dict = user_data['money']
+
+    if str(member.id) not in money_dict:
+            money_dict[member.id] = 0
+
+    for key in ['offences', 'xp']:
+        if str(member.id) not in user_data[key]:
+            user_data[key][str(member.id)] = [0, 0]
+
+    with open(user_data_path, 'w') as user_data_file:       
+        json.dump(user_data, user_data_file)
+
 # takes member to have their balance changed, the amount to add or remove, 
 # the channel so Xi jinping can notify them and add_or_remove which is true 
 # if adding and false if removing
@@ -68,6 +87,9 @@ async def on_member_join(member):
             user_roles = user_data['roles']
     
     
+    add_member_to_json(member)
+    
+    # gives a user their old role if they left previously
     for user in list(user_roles):
         if str(member.id) not in user_roles:
             await member.add_roles(holy_role)
@@ -108,78 +130,190 @@ async def on_member_remove(member):
 
 @bot.event
 async def on_ready():
-    print("I'm ready to ping some pongs")
 
+    # variables for on_message, put here to make it look cleaner
+    global last_author
+    global messages
+    global muterole
+    global last_time
+
+    # getting the server
+    global guild
+
+    guild = bot.get_guild(739522722169618516)
+
+
+    muterole = discord.utils.get(guild.roles, id=739538288255303710)
+    messages = 0
+    last_author = ''
+    last_time = 0
+
+    # variable for free game check
+    global old_games
+    old_games = []
+
+    # generates json
+    for member in guild.members:
+        add_member_to_json(member)
+
+
+        
+    #starts the thursday loop
+    called_on_thursday.start()
+
+    #starts the manage offences loop
+    manage_offences.start()
+
+    print("I'm ready to ping some pongs\n")
 
 #MUTING AND UNMUTING
 #text chat control and muting
 #needs updates for ocmpatibility with xjp2
 @bot.event
 async def on_message(message):
-    global current_time
+
+
+    # variables for later use
     global last_author
     global messages
-    global sender
-    sender = message.author.id
-    messages = ''
-    last_author = ''
-    roles = ""
-    roles = message.author.guild.roles
+    global muterole
+    global last_time
+
+    # getting the author to a shorter name
+    author = message.author
+
+    # getting the role that mutes people
     
-    muterole = discord.utils.get(message.author.roles, id=739538288255303710)
 
-    #loads the userdata json
+    # loading user data
     with open(user_data_path, "r") as user_data_file:
-        userdict = json.load(user_data_file)
+        user_data = json.load(user_data_file)
+        xp_dict = user_data['xp']
+        offences = user_data['offences']
+    
 
-    current_time = time()
+    
 
-    #must be changed i think?
-    # message_channel = bot.get_channel(target_channel_id)
-
-    if not message.author.id == "701139756330778745" and not message.author.id == "706689119841026128" and not message.author.id == "741016411463352362":
-        if time() - current_time < 0.5 and last_author == message.author:
+    # checks that the message author is not a bot
+    if not (author.id in [701139756330778745, 706689119841026128, 741016411463352362]):
+        if time() - last_time < 0.75 and last_author == author.id:
             messages += 1
 
         else:
             messages = 0
-
-        if messages > 5:
+        
+        if messages > 4:
             
-            offencedict = userdict["offences"] #gets the offence dictionary
-            useroffences = offencedict[str(sender)] #gets users current offences    
+            # gets the users current offences
+            current_offences = offences[str(author.id)][0] + 1
+            
+            # gets the time they should be muted for 
+            mute_time = time() + ((current_offences - 1) * 60 + 60)
+            mute_time_in_seconds = mute_time - time()
+            print(f'Muted {author.name} for {mute_time_in_seconds} seconds \n{author.name} has {current_offences} offences')
 
-            print("muted", sender, " for", useroffences * 60 + 60, " seconds") #self explanatory 
-            print(time()) #prints the time
-            print(useroffences) #if you're reading this then you are retarded
-            print(useroffences + 1) #same here
-            await message.author.add_roles(muterole)
-            unbantime[str(sender)] = time() + int(useroffences) * 60 + 60
-            print(unbantime)
+            # informs the traitor
+            await message.channel.send(f"""{author.name.upper()} YOU HAVE MADE A GREVIOUS ERROR, 
+    A GREAT LAPSE IN YOUR JUDGEMENT IF YOU WILL,
+    AND YOU SHALL PAY,
+    YOU HAVE BEEN SILENCED FOR {mute_time_in_seconds} SECONDS!!#!!!@3!3!#@!!!""")
 
-            with open(user_data_path, "w") as user_data_file:
-                json.dump(userdict, user_data_file) #writes the new dict to overrite the old one
 
-        elif messages <= 5:
-            xpdict = userdict["xp"]
-            xp_and_lv = xpdict[str(sender)]
-            experience = xp_and_lv[0]
-            level = xp_and_lv[1]
-            experience += 1
-            if experience >= level ** 2:
-                level += 1
-                await message.channel.send(f'congradulations, you leveled up! your level is now {level}')
-            xp_and_lv[1] = level
-            xp_and_lv[0] = experience
-            xpdict[str(sender)] = xp_and_lv
+            # mutes them
+            await author.add_roles(muterole)
+            
+            offences[str(author.id)] = [current_offences, mute_time]
 
-            with open(user_data_path, "w") as user_data_file:
-                json.dump(userdict, user_data_file) #writes the new dict to overrite the old one
+            with open(user_data_path, 'w') as user_data_file:
+                json.dump(user_data, user_data_file)
+        
 
-    last_author = message.author
-    current_time = time()
+            
 
+    
+    
+    
+    # getting a time to compare to 
+    last_time = time()
+
+    # sets who messaged last
+    last_author = author.id
+
+    # makes it so commands still work
     await bot.process_commands(message)
+
+@bot.command()
+async def number_guess(ctx):
+    
+    #variable declarations including random number
+    play_again = ''
+    guesses = 7
+    number = randint(1, 100)
+    
+    #while loop to play the game
+    while True:
+        
+        #asks the user to guess a number then checks if the next message is from the caller of  the command
+        await ctx.send("Guess a number")
+        guess = await bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
+        
+        #checks if the user has no  guesses left
+        if guesses < 0:
+            
+            #says that the user is a stupid idiot because this game is extremely easy to win
+            await ctx.send(f"Idiot dumb LOSER dumb idiot pogpega not epic gaming not kekaga the number was {number} dumb idiot man")
+            sleep(0.5)
+            await ctx.send("You really shouldn't have lost considering how easy it is to win")
+            sleep(0.5)
+            await ctx.send("Are you really that dumb and that much of a LOSER that you lost at a game popular among 3rd graders?")
+            sleep(0.5)
+            await ctx.send("Play Again? [Y/n]")
+            #asks if the user wants to play again then declares a new number variable if they do want to
+            play_again = await bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
+            
+            if play_again.content.upper() == "Y":
+                number = randint(1, 100)
+                guesses = 7 
+                continue
+            
+            else:
+                break
+            
+            
+        # checks if the user has won 
+        elif int(guess.content) == number:
+            
+            # asks the user if they want to play again     
+            await ctx.send("You won, play again [Y/n]")
+            await change_ping_bucks(ctx.message.author, 10, ctx.message.channel, True)
+            play_again = await bot.wait_for('message', check=lambda x: x.author == ctx.message.author)
+            if play_again.content.upper() == "Y":
+                number = randint(1, 100)
+                guesses = 7
+                continue
+            else:
+                break
+                
+        elif int(guess.content) < number:
+            guesses -= 1
+            await ctx.send("You guessed lower than the epic number")
+            continue
+        else:
+            guesses -= 1 
+            await ctx.send("You guessed Higher than the epic number")
+            continue
+
+@bot.command()
+async def xi_jinping(ctx):
+    
+    #Pings the bot
+    await ctx.send(f"Pong {round(bot.latency * 1000)}ms")
+
+@bot.command()
+async def view_json(ctx):
+    with open('user_data.json') as user_data_file:
+        user_data = json.load(user_data_file)
+        print(json.dumps(user_data, indent=4, sort_keys=True))
 
 @bot.command()
 async def ping_bal(ctx):
@@ -265,15 +399,157 @@ async def whos_that_pokemon(ctx):
             await ctx.send(f"im devistated {sad_zeggy}")
             break
 
+@bot.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def bomb(ctx):
+    await ctx.message.author.voice.channel.connect()
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.message.author.guild)
+    voice.play(discord.FFmpegPCMAudio('Boom.mp3'))
+    voice.volume = 100
+    while voice.is_playing():
+        continue
+    await voice.disconnect()
 
+@bot.command()
+async def test(ctx):
+    global old_games
+
+    # Gets the message channel to send to 
+    message_channel = bot.get_channel(739529274964574360)
+    print(f"Got channel {message_channel}\n")
+    
+    hour = datetime.today().hour
+    # Checks if it is thursday and sends reminder
+    if datetime.today().weekday() == 3 and (hour == 11):
+
+        await message_channel.send("@everyone New Epic Time Gaming Time Epic time Gaming Time Epic free time gaming ")
+
+
+    HEADERS = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
+
+    request = get("https://steamdb.info/sales/", timeout=10, headers=HEADERS)
+
+
+
+    html = BeautifulSoup(request.content, 'html.parser')
+
+    discounts = html.find_all('tr')
+
+    row_lst = [discount for discount in discounts]
+    row_lst.pop(0)
+
+    row_lst_clean = []
+
+
+    for row in row_lst:
+        indivdual_row = []
+        for string in row.stripped_strings:
+            indivdual_row.append(string)
+        row_lst_clean.append(indivdual_row)
+
+
+    
+
+    for row in row_lst_clean:
+        for element in row:
+            
+            if element == '$0.00' and old_games.count(str(row[0])) <= 0:
+                
+                
+                old_games.append(row[0])
+                
+                
+                await message_channel.send(f'@everyone {row[0]} is free on steam')
+
+
+@tasks.loop(hours=1)
+async def called_on_thursday():
+    global old_games
+
+    # Gets the message channel to send to 
+    message_channel = bot.get_channel(739529274964574360)
+    print(f"Got channel {message_channel}\n")
+    
+    hour = datetime.today().hour
+    # Checks if it is thursday and sends reminder
+    if datetime.today().weekday() == 3 and (hour == 11):
+
+        await message_channel.send("@everyone New Epic Time Gaming Time Epic time Gaming Time Epic free time gaming ")
+
+
+    HEADERS = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
+
+    request = get("https://steamdb.info/sales/", timeout=10, headers=HEADERS)
+
+
+
+    html = BeautifulSoup(request.content, 'html.parser')
+
+    discounts = html.find_all('tr')
+
+    row_lst = [discount for discount in discounts]
+    row_lst.pop(0)
+
+    row_lst_clean = []
+
+
+    for row in row_lst:
+        indivdual_row = []
+        for string in row.stripped_strings:
+            indivdual_row.append(string)
+        row_lst_clean.append(indivdual_row)
+
+
+    
+
+    for row in row_lst_clean:
+        for element in row:
+            
+            if element == '$0.00' and old_games.count(str(row[0])) <= 0:
+                
+                
+                old_games.append(row[0])
+                
+                
+                await message_channel.send(f'@everyone {row[0]} is free on steam')
+
+# lowers offences and unmutes people             
+@tasks.loop(seconds=5)
+async def manage_offences():
+
+    global muterole
+    with open(user_data_path, 'r') as user_data_file:
+        user_data = json.load(user_data_file)
+        offences = user_data['offences']
+        
+
+    for member_id in offences:
+        member_value = offences[member_id]
+        
+
+        # removes one offence every 12 hours and 12 PM and AM 
+        if datetime.today().hour % 12 == 0:
+            member_value[0] -= 1 
+            remov
+            
+        if (time() - member_value[1]) > 0 and member_value[1] != 0:
+            member_to_unmute = discord.utils.get(guild.members, id=int(member_id))
+            
+            await member_to_unmute.remove_roles(muterole)
+            member_value[1] = 0
+            print(f'Unmuted {member_to_unmute.name}')
+
+    with open(user_data_path, 'w') as user_data_file:
+        json.dump(user_data, user_data_file)
 
 
 
 f = open("token.txt", "r") # get key
 # Use test token if testing new features
 TOKEN = f.readline()
-print(TOKEN)
+
 TEST_TOKEN = f.readline()
 f.close()
+
 
 bot.run(str(TOKEN))
