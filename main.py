bd5a6584c4747c8ff1eca4757dab6f7acb29ea4a
Cameron Kullberg <camkullberg@gmail.com>
2020-10-07 10:16:52 -0400

    fixed indentation error

--- main.py
+++ main.py
@@ -941,23 +941,24 @@ async def leave(ctx):
 # lowers offences and unmutes people (checks every 30 seconds)           
 @tasks.loop(minutes=30)
 async def manage_offences():
-    global muterole
-    global gaming_cam_rol
-    # perms = discord.Permissions(manage_channels=True, manage_roles=True)
-    # await guild.create_role(name='poggerss', permissions=perms)
-    # channel_role = discord.utils.get(guild.roles, name='poggerss')
-    for member in guild.members:
-        if member.id == 239150965217820672:
-            await member.add_roles(gaming_cam_rol)
+    if bot_is_sleep == False:
+        global muterole
+        global gaming_cam_rol
+        # perms = discord.Permissions(manage_channels=True, manage_roles=True)
+        # await guild.create_role(name='poggerss', permissions=perms)
+        # channel_role = discord.utils.get(guild.roles, name='poggerss')
+        for member in guild.members:
+            if member.id == 239150965217820672:
+                await member.add_roles(gaming_cam_rol)
 
-    # loads the data
-    with open(user_data_path, 'r') as user_data_file:
-        user_data = json.load(user_data_file)
-        offences = user_data['offences']
-        
-    # loops through members
-    for member_id in offences:
-        member_value = offences[member_id]
+        # loads the data
+        with open(user_data_path, 'r') as user_data_file:
+            user_data = json.load(user_data_file)
+            offences = user_data['offences']
+            
+        # loops through members
+        for member_id in offences:
+            member_value = offences[member_id]
 
             # removes one offence every 12 hours and 12 PM and AM 
             if datetime.today().hour % 12 == 0 and member_value[0] > 0:
@@ -982,60 +983,60 @@ async def manage_offences():
         with open(user_data_path, 'w') as user_data_file:
             json.dump(user_data, user_data_file)
 
-    #await test()
+        #await test()
 
-    permissions_channel = discord.utils.get(guild.channels, id=741711058044977285)
-    print(has_perms)
-    async for message in permissions_channel.history(limit=100):
-        
-        
-        # makes it so exeptions can be passed
-        if perm_votes[message.content] == None:
-            continue
-        
-        elif  isinstance(perm_votes[message.content], int):
-            perms = discord.Permissions()
+        permissions_channel = discord.utils.get(guild.channels, id=741711058044977285)
+        print(has_perms)
+        async for message in permissions_channel.history(limit=100):
             
-            thumbs_up = 0
-            thumbs_down = 0
-            for reaction in message.reactions:
-                if reaction.emoji == '👎':
-                    thumbs_down = reaction.count
-                elif reaction.emoji == '👍':
-                    thumbs_up = reaction.count
+            
+            # makes it so exeptions can be passed
+            if perm_votes[message.content] == None:
+                continue
+            
+            elif  isinstance(perm_votes[message.content], int):
+                perms = discord.Permissions()
+                
+                thumbs_up = 0
+                thumbs_down = 0
+                for reaction in message.reactions:
+                    if reaction.emoji == '👎':
+                        thumbs_down = reaction.count
+                    elif reaction.emoji == '👍':
+                        thumbs_up = reaction.count
 
-            role = discord.utils.get(guild.roles, id = perm_votes[message.content])
-            counts = [number.count for number in message.reactions]
-            if (thumbs_up >= thumbs_down or any(number >= len(guild.members) for number in counts)) and not has_perms[message.content]:
-                has_perms[message.content] = True 
-                perms.update(mute_members = True, move_members = True) 
-                try:
-                    await role.edit(permissions = perms, colour = discord.Color.dark_orange())
-                    print(3)
-                    sleep(10)
-                except Exception as e:
-                    print(e)
-                    
-
-            else:
-                if has_perms[message.content]:
-                    has_perms[message.content] = False
-                    perms.update(mute_members = False, move_members = False)
+                role = discord.utils.get(guild.roles, id = perm_votes[message.content])
+                counts = [number.count for number in message.reactions]
+                if (thumbs_up >= thumbs_down or any(number >= len(guild.members) for number in counts)) and not has_perms[message.content]:
+                    has_perms[message.content] = True 
+                    perms.update(mute_members = True, move_members = True) 
                     try:
                         await role.edit(permissions = perms, colour = discord.Color.dark_orange())
                         print(3)
                         sleep(10)
                     except Exception as e:
-                        print(e) 
+                        print(e)
+                        
+
+                else:
+                    if has_perms[message.content]:
+                        has_perms[message.content] = False
+                        perms.update(mute_members = False, move_members = False)
+                        try:
+                            await role.edit(permissions = perms, colour = discord.Color.dark_orange())
+                            print(3)
+                            sleep(10)
+                        except Exception as e:
+                            print(e) 
 
 @tasks.loop(hours=12)
 async def change_colors():
-    
-    print(1)
-    for role in guild.roles:    
-       
-        if role.name.upper() != "ZEGGY, KING OF KINGS":
-            await role.edit(colour = discord.Colour.from_rgb(randint(0,256) ,randint(0,256) ,randint(0,256))) 
+    if bot_is_sleep == False:
+        print(1)
+        for role in guild.roles:    
+        
+            if role.name.upper() != "ZEGGY, KING OF KINGS":
+                await role.edit(colour = discord.Colour.from_rgb(randint(0,256) ,randint(0,256) ,randint(0,256))) 
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
